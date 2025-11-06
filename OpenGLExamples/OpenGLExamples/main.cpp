f9c2805504b934d5a2676dc3b2803452399f9f32
horolock96 <horolock96@gmail.com>
2025-11-06 22:09:42 +0900

    Add some debug logics

--- OpenGLExamples/OpenGLExamples/main.cpp
+++ OpenGLExamples/OpenGLExamples/main.cpp
@@ -33,15 +33,37 @@ GLfloat vertColor[] = {
 	0.0F, 0.0F, 1.0F, 1.0F,
 };
 
+void DebugLog(
+	GLenum source, 
+	GLenum type, 
+	GLuint id, 
+	GLenum severity, 
+	GLsizei length,
+	const GLchar* message, 
+	const GLvoid* userParam
+) {
+	std::cout << "Type: " << type << "; Source: " << source << "; ID: " << id << "; Sevirity: " << severity << std::endl;
+	std::cout << "Message: " << message << std::endl;
+	fflush(stdout);
+}
+
 void initFunc(void) {
 	const std::string vertSource = loadFile(vertFileName);
 	const std::string fragSource = loadFile(fragFileName);
+
+	char buf[1024];
+	GLint status;
 
 	// Vertex Shader
 	const char* vertSourcePtr = vertSource.c_str();
 	vert = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vert, 1, &vertSourcePtr, NULL);
 	glCompileShader(vert);
+
+	glGetShaderiv(vert, GL_COMPILE_STATUS, &status);
+	std::cout << "Vert Compile Status = " << ((status == GL_TRUE) ? "true" : "false") << std::endl;
+	glGetShaderInfoLog(vert, sizeof(buf), NULL, buf);
+	std::cout << "Vert Log = [" << buf << "]" << std::endl;
 
 	// Fragment Shader
 	const char* fragSourcePtr = fragSource.c_str();
@@ -99,10 +121,17 @@ void keyFunc(GLFWwindow* window, int key, int scancode, int action, int mods) {
 
 int main(int argc, char* argv[]) {
 	glfwInit();
+
 	GLFWwindow* window = glfwCreateWindow(WIN_W, WIN_H, "OpenGL Example", NULL, NULL);
 	glfwSetWindowPos(window, WIN_X, WIN_Y);
 	glfwMakeContextCurrent(window);
 	glewInit();
+
+	// Register Debug Callback
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
+	glDebugMessageCallback(DebugLog, NULL);
 
 	// Prepare
 	glfwSetWindowRefreshCallback(window, refreshFunc);
